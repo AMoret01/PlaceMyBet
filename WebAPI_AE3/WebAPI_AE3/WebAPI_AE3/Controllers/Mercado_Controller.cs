f52ecd54b7f8f16ca0f68cea3714fb48ff617209
AMoret01 <almogo01@floridauniversitaria.es>
2020-11-02 19:29:48 +0100

    AE3 con bin

---
+++ WebAPI_AE3/WebAPI_AE3/WebAPI_AE3/Controllers/Mercado_Controller.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.UI;
+using WebAPI_AE3.Models;
+
+namespace WebAPI_AE3.Controllers
+{
+    [Route("api/Mercado/{action}")]
+    public class MercadoController : ApiController
+    {
+        // GET: api/Mercado
+        [HttpGet]
+        [ActionName("Get")]
+        public IEnumerable<Mercado> Get()
+        {
+            Repositorio_Mercado repository = new Repositorio_Mercado();
+            List<Mercado> mercados = repository.retrieve();
+            return mercados;
+        }
+
+        [HttpGet]
+        [ActionName("GetDTO")]
+        public IEnumerable<MercadoDTO> GetDTO()
+        {
+            var repository = new Repositorio_Mercado();
+            List<MercadoDTO> mercados = repository.retrieveDTO();
+            return mercados;
+        }
+
+        // GET: api/Mercado/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Mercado
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Mercado/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/Mercado/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
